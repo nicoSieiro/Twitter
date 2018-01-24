3a22798602147cc69496785cd1e1162f6cecfd3b
nicoSieiro <sieiro.nicolas@gmail.com>
2018-01-24 15:22:22 -0300

    root, express.static

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
+const routes = require('./routes');
 const express = require( 'express' );
 const app = express(); // crea una instancia de una aplicación de express
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const nunjucks = require('nunjucks')
 const fs = require('fs')
+const path = require('path')
 
+app.use('/', routes);
 
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'), '-',
+//       tokens['response-time'](req, res), 'ms'
+//     ].join(' ')
+// }))
 
 //nunjucks:
 var locals = {
@@ -29,18 +34,5 @@ app.set('view engine', 'html'); // hace que res.render funcione con archivos htm
 app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
 nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
 
-
-app.get('/',function(req,res){
-    res.render('index.html', locals)
-})
-
-app.get('/gary',function(req,res){
-    res.send("hola gary")
-})
-
-app.post('/gary',function(req,res){
-    res.send("puto el qque lee")
-    
-})
 
 app.listen(3000)
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+// Se puede usar solo una linea: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+
+
+
+
+module.exports = router;
